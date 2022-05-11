fe20ff5bc74cfe48c340ca5f8242abcae2ffb8cd
Patrick Southier <patrick.southier@hotmail.com>
2022-05-11 15:51:18 -0300

    feat: creating the buyers table seeder file

---
+++ server/seeders/20220511183339-buyers.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Buyers', [
+      {
+        name: 'SACADO 001',
+        tradingName: 'SACADO 001 LTDA',
+        cashforceTax: '0',
+        createdAt: new Date('2020-10-29T21:20:33'),
+        updatedAt: new Date('2020-10-29T21:20:34'),
+        cnpjId: 1,
+        confirm: 1,
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Buyers', null, {});
+  }
+};
