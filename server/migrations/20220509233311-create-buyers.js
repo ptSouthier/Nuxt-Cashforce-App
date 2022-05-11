bcc101cb332433b79f6a18b5a266d59781a81423
Patrick Southier <patrick.southier@hotmail.com>
2022-05-11 20:55:19 -0300

    refactor: changing the onDelete property of cnpjId column in order to be able to run the migration

--- server/migrations/20220509233311-create-buyers.js
+++ server/migrations/20220509233311-create-buyers.js
@@ -83,7 +83,7 @@ module.exports = {
           key: 'id',
         },
         onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
+        onDelete: 'CASCADE',
       },
       confirm: {
         type: Sequelize.BOOLEAN,
