4fecf57d1f1700a8aa1001dceb93d5f8a9a1532d
Patrick Southier <patrick.southier@hotmail.com>
2022-05-11 15:26:54 -0300

    refactor: adding await before the queryInterface actions in order to solve the problem pointed out by eslint

--- server/migrations/20220509233311-create-buyers.js
+++ server/migrations/20220509233311-create-buyers.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    const BuyersTable = queryInterface.createTable('Buyers', {
+    const BuyersTable = await queryInterface.createTable('Buyers', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -90,6 +90,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    queryInterface.dropTable('Buyers');
+    await queryInterface.dropTable('Buyers');
   }
 };
