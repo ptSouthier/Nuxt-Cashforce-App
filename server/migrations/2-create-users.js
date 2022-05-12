8d4ef343800e60972095a4b377a18cb5749ae425
Patrick Southier <patrick.southier@hotmail.com>
2022-05-11 22:01:21 -0300

    feat: adding the unique constraint at email column on users table

--- server/migrations/2-create-users.js
+++ server/migrations/2-create-users.js
@@ -16,6 +16,7 @@ module.exports = {
       email: {
         allowNull: false,
         type: Sequelize.STRING,
+        unique: true,
       },
       phoneNumber: {
         type: Sequelize.STRING,
