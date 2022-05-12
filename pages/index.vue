2aff83da28572c8a8b520db7846448ded9b69ee6
Patrick Southier <patrick.southier@hotmail.com>
2022-05-12 19:39:56 -0300

    feat: deleting the Tutorial.vue component and changing the index.vue page just to test if we are receiving the right data from API

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,23 @@
 <template>
-  <Tutorial />
+  <ul>
+    <li v-for="order in orders" :key="order.id">
+      <h2>{{ order.orderNfId }}</h2>
+    </li>
+  </ul>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage',
-}
+import Vue from "vue";
+
+export default Vue.extend({
+  async asyncData({ $axios }) {
+    const orders = await $axios.$get('/orders');
+    return { orders };
+  },
+  data() {
+    return {
+      orders: [],
+    }
+  }
+})
 </script>
