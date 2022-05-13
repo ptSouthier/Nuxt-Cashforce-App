4049733fb124585c8c2b066c6ccc8f749959638e
Patrick Southier <patrick.southier@hotmail.com>
2022-05-13 05:49:49 -0300

    creating the OrdersTable component and adding it on index page file with its props

---
+++ components/OrdersTable.vue
@@ -0,0 +1,61 @@
+<template>
+  <table class="table-auto border-separate">
+    <thead>
+      <tr>
+        <th class="table-header-style">NOTA FISCAL</th>
+        <th class="table-header-style">SACADO</th>
+        <th class="table-header-style">CEDENTE</th>
+        <th class="table-header-style">EMISSÃO</th>
+        <th class="table-header-style">VALOR</th>
+        <th class="table-header-style">STATUS</th>
+        <th class="table-header-style"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="order in orders" :key="order.id" class="shadow rounded-sm h-11">
+        <td class="table-data-style">{{ order.orderNfId }}</td>
+        <td class="table-data-style">{{ order.buyerId }}</td>
+        <td class="table-data-style">{{ order.providerId }}</td>
+        <td class="table-data-style">{{ order.emissionDate }}</td>
+        <td class="table-data-style green-text">{{ order.value }}</td>
+        <td class="table-data-style green-text">{{ order.orderStatusBuyer }}</td>
+        <td class="table-data-style">
+          <button>Dados do cedente</button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+  import Vue from "vue";
+
+  export default Vue.extend({
+    props: {
+      orders: { type: Array, required: true },
+    },
+  })
+</script>
+
+<style scoped>
+  table {
+    border-spacing: 1rem;
+  }
+
+  .table-header-style {
+    @apply w-60 text-gray-400 text-xs font-semibold;
+  }
+
+  .table-data-style {
+    @apply w-60 p-1 text-gray-800 text-sm text-center font-medium;
+  }
+
+  .green-text {
+    @apply text-green-500;
+  }
+
+  button {
+    @apply rounded-full border px-3 py-1 border-blue-400 font-medium text-gray-700;
+    font-size: 0.7rem;
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,18 @@
   <div class="flex items-start flex-col">
     <HeaderBar />
     <SideBar />
+
     <div class="ml-72">
       <h1 class="heading-text">
         <img src="../assets/proposta.svg" alt="Ícone de aperto de mãos" class="w-6 h-12 mr-2">
         Notas fiscais
       </h1>
       <span class="text-gray-400 ml-12 font-semibold text-sm">Visualize as notas fiscais que você tem.</span>
-      <ul>
-        <li v-for="order in orders" :key="order.id">
-          <h2>{{ order.orderNfId }}</h2>
-        </li>
-      </ul>
+
+      <div class="p-10">
+        <OrdersTable :orders="orders" />
+      </div>
+
     </div>
   </div>
 </template>
