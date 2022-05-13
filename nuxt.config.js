322cc681b6893604d3bbdd057919a2ded619a0c2
Patrick Southier <patrick.southier@hotmail.com>
2022-05-13 13:33:02 -0300

    chore: removing comment lines from nuxt.config.js file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Cashforce App',
     htmlAttrs: {
@@ -16,34 +15,25 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    // https://go.nuxtjs.dev/eslint
     '@nuxtjs/eslint-module',
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
+
     '@nuxtjs/axios',
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     baseURL: `http://localhost:${process.env.API_PORT || 3001}`,
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
 }
