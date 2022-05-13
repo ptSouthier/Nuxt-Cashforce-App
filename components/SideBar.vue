97756fb4ad9ca5c4b711ff882aeedfabf6c11cab
Patrick Southier <patrick.southier@hotmail.com>
2022-05-12 23:29:25 -0300

    feat: creating the SideBar component

---
+++ components/SideBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sider-financiador auto-layout bg-gray-50">
+    <div class="ml-2">
+      <img src="../assets/logo.svg" class="p-5">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .auto-layout {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .sider-financiador {
+    position: absolute;
+    width: 18rem;
+    height: 100%;
+    left: 0px;
+    top: 0px;
+  }
+</style>
