15e02e985e008160c8ddba4dacdb06a17e846d0c
Patrick Southier <patrick.southier@hotmail.com>
2022-05-13 00:42:33 -0300

    feat: adding the navigation element and the "notas fiscais" button

--- components/SideBar.vue
+++ components/SideBar.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="sider-financiador auto-layout bg-gray-50">
-    <div class="ml-2">
-      <img src="../assets/logo.svg" class="p-5">
+    <div>
+      <img src="../assets/logo.svg" alt="Cashforce Logo" class="p-5 ml-2 h-16">
+      <nav class="flex items-center pt-5">
+        <button class="nav-button">
+          <img src="../assets/proposta.svg" alt="Ícone de aperto de mãos" class="w-6 h-12 ml-5">
+          <span class="ml-3 font-semibold text-sm">Notas fiscais</span>
+        </button>
+      </nav>
     </div>
   </div>
 </template>
@@ -18,5 +24,9 @@
     height: 100%;
     left: 0px;
     top: 0px;
+  }
+
+  .nav-button {
+    @apply flex items-center text-gray-700 border-l-4 hover:text-green-500 hover:border-green-500;
   }
 </style>
