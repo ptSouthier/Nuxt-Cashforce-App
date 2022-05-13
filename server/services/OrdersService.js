96fe7d39d014d9be05b5fb41077284fe39d5de2f
Patrick Southier <patrick.southier@hotmail.com>
2022-05-13 06:33:41 -0300

    feat: implementing business rules to the OrdersService file in order to make the API return the processed data

--- server/services/OrdersService.js
+++ server/services/OrdersService.js
@@ -1,8 +1,33 @@
 const { StatusCodes } = require('http-status-codes');
 const { Order } = require('../models');
+const orderStatusBuyerDescription = [
+  'Pendente de confirmação',
+  'Pedido confirmado',
+  'Não reconhece o pedido',
+  'Mercadoria não recebida',
+  'Recebida com avaria',
+  'Devolvida',
+  'Recebida com devolução parcial',
+  'Recebida e confirmada',
+  'Pagamento Autorizado',
+];
 
 const getAll = async () => {
   const orders = await Order.findAll({});
+
+  orders.map((order) => {
+    const orderStatusBuyerInteger = parseInt(order.orderStatusBuyer);
+    const valueInteger = parseInt(order.value);
+    const formatedEmissionDate = new Date(order.emissionDate).toLocaleDateString('pt-br');
+    const valueToReal = valueInteger.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
+
+    order.orderStatusBuyer = orderStatusBuyerDescription[orderStatusBuyerInteger];
+    order.emissionDate = formatedEmissionDate;
+    order.value = valueToReal;
+
+    return order;
+  });
+
   return { status: StatusCodes.OK, data: orders };
 };
 
