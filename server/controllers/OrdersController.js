db83a90673d7900fb14b1cafb21db570ff51b55e
Patrick Southier <patrick.southier@hotmail.com>
2022-05-23 20:33:45 -0300

    feat: implementing and exporting the getById function at OrdersController file

--- server/controllers/OrdersController.js
+++ server/controllers/OrdersController.js
@@ -6,6 +6,18 @@ const getAll = async (_req, res) => {
   res.status(status).json(data);
 };
 
+const getById = async (req, res) => {
+  const { id } = req.params;
+  const { status, data, message } = await Orders.getById(id);
+
+  if (message) {
+    return res.status(status).json({ message });
+  };
+
+  res.status(status).json(data);
+};
+
 module.exports = {
   getAll,
+  getById,
 };
