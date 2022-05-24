7a31497a97ea37062ae8cdb0d060297418a9083f
Patrick Southier <patrick.southier@hotmail.com>
2022-05-23 23:43:56 -0300

    feat: adding the /users endpoint with its router

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const OrdersRouter = require('./routes/OrdersRouter');
+const UsersRouter = require('./routes/UsersRouter');
 require('dotenv').config();
 
 const PORT = process.env.API_PORT || 3001;
@@ -14,3 +15,4 @@ app.listen(PORT, () => {
 });
 
 app.use('/orders', OrdersRouter);
+app.use('/users', UsersRouter);
